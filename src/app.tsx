220923a10710492f9f52c84e465ea718c3b8a925
Håvard Høiby <havard.hoiby@autodesk.com>
2023-12-01 00:15:13 +0100

    feat: improved chat bot

--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useState } from "preact/hooks";
+import { useEffect, useState } from "preact/hooks";
 import ChatBot from "react-simple-chatbot";
 import { Forma } from "forma-embedded-view-sdk/auto";
 import * as THREE from "three";
@@ -6,15 +6,6 @@ import { GLTFExporter } from "three/examples/jsm/exporters/GLTFExporter.js";
 
 window.THREE = THREE;
 window.GLTFExporter = GLTFExporter;
-
-function base64ToArrayBuffer(base64: string) {
-  const binaryString = atob(base64);
-  const bytes = new Uint8Array(binaryString.length);
-  for (let i = 0; i < binaryString.length; i++) {
-    bytes[i] = binaryString.charCodeAt(i);
-  }
-  return bytes.buffer;
-}
 window.Forma = Forma;
 
 const SHOULD_MOCK = false;
@@ -43,33 +34,16 @@ const mockResponse = {
   system_fingerprint: null,
 };
 
-function renderPromptTHREE(prompt: string, selectedSite: [number, number][], elevation: number) {
-  return `
-   Given the site polygon ${JSON.stringify(selectedSite)}
-
-The z elevation of the terrain is ${elevation} meters.
-
-Can you create a function called generate in javascript.
-Create a threejs scene.
-Add ${prompt} to the scene inside the site boundary.
-These boxes should be at the terrain elevation.
-Do not add the site boundary to the scene.
-return the scene
-
-   Make THREE parameter to the generate function
-
-   You do not need to render just export the scene :) 
-   Do not add any usage examples.
-   
-   Expose the function on window.generate
-   `;
-}
-
 function renderPrompt(prompt: string, selectedSite: [number, number][]) {
   return `
 Given the site boundary ${JSON.stringify(selectedSite)}
 
-Create ${prompt} inside the site boundary without overlapping and with a minimum of 5 meter between each other as geojson. For each building return a polygon with a height property.
+Create ${prompt} inside the site boundary without overlapping and with a minimum of 5 meter between each other as geojson. 
+For each building return a feature with type "Polygon" with a height and a numberOfFloors property.
+For each road return a feature with type LineString.
+
+Do not include buildings if they are not mentioned in the prompt.
+Do not include roads if they are not mentioned in the prompt.
 
 Not in javascript print the geojson.
 Print the resulting geojson as a FeatureCollection with features
@@ -78,25 +52,33 @@ Print the resulting geojson as a FeatureCollection with features
 
 type Geojson = {
   type: "FeatureCollection";
-  features: {
-    type: "Feature";
-    geometry: {
-      type: "Polygon";
-      propeties: { height: number };
-      coodinates: [number, number][][];
-    };
-  }[];
+  features: (
+    | {
+        type: "Feature";
+        properties: { height: number };
+        geometry: {
+          type: "Polygon";
+          coordinates: [number, number][][];
+        };
+      }
+    | {
+        type: "Feature";
+        geometry: {
+          type: "LineString";
+          coordinates: [number, number][];
+        };
+      }
+  )[];
 };
 
-function extractGeojson(response: any) {
+function extractGeojson(response: any): Geojson {
   const [choice] = response.choices;
 
   return JSON.parse(choice?.message.content);
 }
 
-async function callChatGPT(prompt: string) {
+async function callChatGPT(prompt: string, apiKey: string) {
   const apiUrl = "https://api.openai.com/v1/chat/completions";
-  const apiKey = "";
   const messages = [
     { role: "system", content: "What do you want to draw?" },
     { role: "user", content: prompt },
@@ -130,42 +112,80 @@ async function callChatGPT(prompt: string) {
 
     const { features } = extractGeojson(result);
 
-    console.log({ features });
-
     for (let feature of features) {
-      console.log(feature.geometry.coordinates);
-      let terrainElevation = await Forma.terrain.getElevationAt({
-        x: feature.geometry.coordinates[0][0][0],
-        y: feature.geometry.coordinates[0][0][0],
-      });
-      for (let [x, y] of feature.geometry.coordinates[0]) {
-        const elevation = await Forma.terrain.getElevationAt({ x, y });
+      if (feature.geometry.type === "Polygon") {
+        let terrainElevation = await Forma.terrain.getElevationAt({
+          x: feature.geometry.coordinates[0][0][0],
+          y: feature.geometry.coordinates[0][0][0],
+        });
+        for (let [x, y] of feature.geometry.coordinates[0]) {
+          const elevation = await Forma.terrain.getElevationAt({ x, y });
 
-        if (elevation < terrainElevation) {
-          terrainElevation = elevation;
+          if (elevation < terrainElevation) {
+            terrainElevation = elevation;
+          }
         }
-      }
 
-      const floorStack = {
-        floors: [
-          {
-            polygon: feature.geometry.coordinates[0],
-            height: feature.properties.height,
-          },
-        ],
-      };
+        let floorStack = {};
 
-      const { urn } = await Forma.elements.floorStack.createFromFloors(floorStack);
+        if (feature?.properties?.numberOfFloors) {
+          const floors = [];
+          for (let i = 0; i < feature?.properties?.numberOfFloors; i++) {
+            floors.push({
+              polygon: feature.geometry.coordinates[0],
+              height: (feature?.properties?.height || 10) / feature?.properties?.numberOfFloors,
+            });
+          }
 
-      // prettier-ignore
-      const transform = [
+          floorStack = {
+            floors,
+          };
+        } else {
+          floorStack = {
+            floors: [
+              {
+                polygon: feature.geometry.coordinates[0],
+                height: feature?.properties?.height || 10,
+              },
+            ],
+          };
+        }
+
+        const { urn } = await Forma.elements.floorStack.createFromFloors(floorStack);
+
+        // prettier-ignore
+        const transform = [
         1, 0, 0, 0, 
         0, 1, 0, 0, 
         0, 0, 1, 0, 
         0, 0, terrainElevation, 1
       ];
 
-      await Forma.proposal.addElement({ urn, transform });
+        await Forma.proposal.addElement({ urn, transform });
+      } else {
+        const { urn } = await Forma.integrateElements.createElementHierarchy({
+          data: {
+            rootElement: "root",
+            elements: {
+              root: {
+                id: "root",
+                properties: {
+                  geometry: {
+                    type: "Inline",
+                    format: "GeoJSON",
+                    geoJson: {
+                      type: "FeatureCollection",
+                      features: [feature],
+                    },
+                  },
+                },
+              },
+            },
+          },
+        });
+
+        await Forma.proposal.addElement({ urn });
+      }
     }
   } catch (error) {
     console.error("Error making API request:", error);
@@ -175,32 +195,35 @@ async function callChatGPT(prompt: string) {
 
 function AskGPT({ steps, triggerNextStep }) {
   const prompt = steps?.prompt?.value;
+  const key = steps?.inputkey?.value;
 
   useEffect(() => {
     (async function call() {
-      const site = await Forma.geometry.getFootprint({
-        path: (await Forma.selection.getSelection())[0],
-      });
+      try {
+        const site = await Forma.geometry.getFootprint({
+          path: (await Forma.selection.getSelection())[0],
+        });
 
-      const [x, y] = site?.coordinates[0];
-
-      let elevation = await Forma.terrain.getElevationAt({ x, y });
-      for (let [x, y] of site?.coordinates) {
-        const thisElevation = await Forma.terrain.getElevationAt({ x, y });
-
-        if (thisElevation < elevation) {
-          elevation = thisElevation;
+        if (site) {
+          await callChatGPT(renderPrompt(prompt, site?.coordinates), key);
         }
+        triggerNextStep({ value: "success" });
+        return;
+      } catch (error) {
+        console.error(error);
+        triggerNextStep({ value: "failed" });
+        return;
       }
-
-      await callChatGPT(renderPrompt(prompt, site?.coordinates, elevation));
-      triggerNextStep();
     })();
   }, []);
 
   return (
     <>
-      <div>{JSON.stringify(steps)}</div>
+      <div>
+        <h1>Thinking ... </h1>
+        This can take up to 2 minutes. Please wait. If you think it failed close the Extension and
+        try again.
+      </div>
     </>
   );
 }
@@ -211,8 +234,82 @@ export default function App() {
       <ChatBot
         steps={[
           {
+            id: "0",
+            message: "Welcome to the Forma ChatBot!",
+            trigger: "getname",
+          },
+          {
+            id: "getname",
+            message: "What is your name?",
+            trigger: "inputname",
+          },
+          {
+            id: "inputname",
+            user: true,
+            trigger: "displayname",
+          },
+          {
+            id: "displayname",
+            message: "Hi {previousValue}, nice to meet you!",
+            trigger: "shorthelp",
+          },
+          {
+            id: "shorthelp",
+            message: "I know how to generate buildings and roads. An example prompt would be: ",
+            trigger: "example1",
+          },
+          {
+            id: "example1",
+            message:
+              "five buildings on a row with a height of 10, 11, 12, 10, 12 meters and 10 floors each",
+            trigger: "whattodo",
+          },
+          {
+            id: "whattodo",
+            message: "What do you want to do?",
+            trigger: "generate-or-learn",
+          },
+          {
+            id: "generate-or-learn",
+            options: [
+              { value: 1, label: "Learn more", trigger: "longhelp" },
+              { value: 2, label: "Generate", trigger: "getkey" },
+            ],
+          },
+          {
+            id: "longhelp",
+            message:
+              "I know about the words building, road, height, and floors. Some example prompts are:",
+            trigger: "example2",
+          },
+          {
+            id: "example2",
+            message: "- a building with 5 floors and 2.5 floor height",
+            trigger: "example3",
+          },
+          {
+            id: "example3",
+            message: "- a 20x20x10 building in each corner of the site",
+            trigger: "example4",
+          },
+          {
+            id: "example4",
+            message: "- a pentagon building in the center of the site",
+            trigger: "getkey",
+          },
+          {
+            id: "getkey",
+            message: "First, I need your ChatGPT API key?",
+            trigger: "inputkey",
+          },
+          {
+            id: "inputkey",
+            user: true,
+            trigger: "1",
+          },
+          {
             id: "1",
-            message: "What do you want to draw?",
+            message: "Select a site limit to generate on and type what to generate?",
             trigger: "prompt",
           },
           {
@@ -224,10 +321,20 @@ export default function App() {
             id: "3",
             component: <AskGPT />,
             waitAction: true,
-            trigger: "4",
+            trigger: ({ value }) => (value === "failed" ? "failed" : "scuccess"),
           },
           {
-            id: "4",
+            id: "failed",
+            message: "Ops! That failed. Did you remeber to select a site limit? Try again",
+            trigger: "1",
+          },
+          {
+            id: "missing-selection",
+            message: "You forgot to select a site limit. Click one in the scene and try again!",
+            trigger: "1",
+          },
+          {
+            id: "scuccess",
             message: "Yay!",
             trigger: "1",
           },
