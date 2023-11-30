2b30af084d873ac02c71b75511b59afcab7e83cf
Håvard Høiby <havard.hoiby@autodesk.com>
2023-11-30 17:58:48 +0100

    make it work

--- src/app.tsx
+++ src/app.tsx
@@ -4,6 +4,9 @@ import { Forma } from "forma-embedded-view-sdk/auto";
 import * as THREE from "three";
 import { GLTFExporter } from "three/examples/jsm/exporters/GLTFExporter.js";
 
+window.THREE = THREE;
+window.GLTFExporter = GLTFExporter;
+
 function base64ToArrayBuffer(base64: string) {
   const binaryString = atob(base64);
   const bytes = new Uint8Array(binaryString.length);
@@ -17,9 +20,9 @@ window.Forma = Forma;
 const SHOULD_MOCK = false;
 
 const mockResponse = {
-  id: "chatcmpl-8QZiU1aiJ0jhVkghlKNTMqID7IiPH",
+  id: "chatcmpl-8QcLbnFMkpSFr4r5YSuDrK3KH0mmr",
   object: "chat.completion",
-  created: 1701344514,
+  created: 1701354627,
   model: "gpt-3.5-turbo-0613",
   choices: [
     {
@@ -27,23 +30,33 @@ const mockResponse = {
       message: {
         role: "assistant",
         content:
-          "Certainly! Here's an example of how you can create a JavaScript function using Three.js and GLTFExporter to build a `<canvas>` in glb format and return it as a base64 encoded string:\n\n```javascript\nwindow.generate = function() {\n  // Create a scene and add objects\n\n  const scene = new THREE.Scene();\n\n  const geometry = new THREE.BoxGeometry(1, 1, 1);\n  const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });\n  const cube = new THREE.Mesh(geometry, material);\n  scene.add(cube);\n\n  const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);\n  camera.position.z = 5;\n\n  // Create a renderer\n\n  const renderer = new THREE.WebGLRenderer({ antialias: true });\n  renderer.setSize(window.innerWidth, window.innerHeight);\n  document.body.appendChild(renderer.domElement);\n\n  // Create a GLTFExporter instance and export the scene\n\n  const exporter = new THREE.GLTFExporter();\n\n  return new Promise((resolve) => {\n    exporter.parse(scene, (glb) => {\n      // Encode the glb as base64\n\n      const glbString = JSON.stringify(glb);\n      const glbBytes = new TextEncoder().encode(glbString);\n      const glbBase64 = btoa(String.fromCharCode(...glbBytes));\n\n      resolve(glbBase64);\n    });\n  });\n};\n```\n\nYou can call this function `generate()` to get a base64 encoded string of the scene in glb format. Note that this function returns a promise, so you might need to handle it accordingly. Also, make sure you have the necessary imports and dependencies (e.g., Three.js, GLTFExporter) set up in your project.",
+          "Sure! Here's a JavaScript function that generates a Three.js scene with two boxes placed inside the given site boundary at the terrain elevation:\n\n```javascript\nfunction generate(site) {\n  const { WebGLRenderer, Scene, PerspectiveCamera, BoxGeometry, MeshBasicMaterial, Mesh } = THREE;\n\n  const renderer = new WebGLRenderer();\n  renderer.setSize(window.innerWidth, window.innerHeight);\n  document.body.appendChild(renderer.domElement);\n\n  const scene = new Scene();\n  const camera = new PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);\n  camera.position.z = 5;\n\n  const siteBoundary = new BoxGeometry(1, 1, 1);\n  const siteMaterial = new MeshBasicMaterial({ color: 0x0000ff });\n  const siteMesh = new Mesh(siteBoundary, siteMaterial);\n  scene.add(siteMesh);\n\n  const boxGeometry1 = new BoxGeometry(1, 1, 1);\n  const boxMaterial1 = new MeshBasicMaterial({ color: 0xff0000 });\n  const box1 = new Mesh(boxGeometry1, boxMaterial1);\n  box1.position.set(-4, -4, 5.834311802022915);\n  scene.add(box1);\n\n  const boxGeometry2 = new BoxGeometry(1, 1, 1);\n  const boxMaterial2 = new MeshBasicMaterial({ color: 0x00ff00 });\n  const box2 = new Mesh(boxGeometry2, boxMaterial2);\n  box2.position.set(4, 4, 5.834311802022915);\n  scene.add(box2);\n\n  return scene;\n}\n\nwindow.generate = generate;\n```\n\nPlease note that this code assumes you have included the Three.js library in your project. You also need to have a working HTML file to run this code.",
       },
       finish_reason: "stop",
     },
   ],
   usage: {
-    prompt_tokens: 72,
-    completion_tokens: 369,
-    total_tokens: 441,
+    prompt_tokens: 214,
+    completion_tokens: 378,
+    total_tokens: 592,
   },
   system_fingerprint: null,
 };
 
-function renderPrompt(prompt: string) {
+function renderPromptTHREE(prompt: string, selectedSite: [number, number][], elevation: number) {
   return `
-   Can you create a function called generate in javascript build a ${prompt} in glb format using threejs and return the glb as a base64 encoded string?
-   Make THREE and GTLFExporter parameters to the generate function
+   Given the site polygon ${JSON.stringify(selectedSite)}
+
+The z elevation of the terrain is ${elevation} meters.
+
+Can you create a function called generate in javascript.
+Create a threejs scene.
+Add ${prompt} to the scene inside the site boundary.
+These boxes should be at the terrain elevation.
+Do not add the site boundary to the scene.
+return the scene
+
+   Make THREE parameter to the generate function
 
    You do not need to render just export the scene :) 
    Do not add any usage examples.
@@ -52,20 +65,38 @@ function renderPrompt(prompt: string) {
    `;
 }
 
-function extractCode(response: any) {
-  const [choice] = response.choices;
-  const regex = /```(?:javascript|js)\n([\s\S]+?)```/g;
+function renderPrompt(prompt: string, selectedSite: [number, number][]) {
+  return `
+Given the site boundary ${JSON.stringify(selectedSite)}
 
-  return choice?.message.content
-    .match(regex)[0]
-    .replace("```javascript", "")
-    .replace("```", "")
-    .replace("```js", "");
+Create ${prompt} inside the site boundary without overlapping and with a minimum of 5 meter between each other as geojson. For each building return a polygon with a height property.
+
+Not in javascript print the geojson.
+Print the resulting geojson as a FeatureCollection with features
+   `;
+}
+
+type Geojson = {
+  type: "FeatureCollection";
+  features: {
+    type: "Feature";
+    geometry: {
+      type: "Polygon";
+      propeties: { height: number };
+      coodinates: [number, number][][];
+    };
+  }[];
+};
+
+function extractGeojson(response: any) {
+  const [choice] = response.choices;
+
+  return JSON.parse(choice?.message.content);
 }
 
 async function callChatGPT(prompt: string) {
   const apiUrl = "https://api.openai.com/v1/chat/completions";
-  const apiKey = "API_KEY";
+  const apiKey = "";
   const messages = [
     { role: "system", content: "What do you want to draw?" },
     { role: "user", content: prompt },
@@ -80,8 +111,10 @@ async function callChatGPT(prompt: string) {
     method: "POST",
     headers: headers,
     body: JSON.stringify({
-      model: "gpt-3.5-turbo",
+      model: "gpt-4-1106-preview",
       messages: messages,
+      response_format: { type: "json_object" },
+      seed: 0,
     }),
   };
 
@@ -95,16 +128,45 @@ async function callChatGPT(prompt: string) {
       result = await response.json();
     }
 
-    const code = extractCode(result);
-    console.log(code);
-    // Handle the result and display the AI response in your UI
-    eval(code);
+    const { features } = extractGeojson(result);
 
-    //@ts-ignore
-    const raw = await window.generate(THREE, GLTFExporter);
-    const glb = base64ToArrayBuffer(raw.replace("data:model/gltf-binary;base64,", ""));
+    console.log({ features });
 
-    Forma.render.glb.add({ glb });
+    for (let feature of features) {
+      console.log(feature.geometry.coordinates);
+      let terrainElevation = await Forma.terrain.getElevationAt({
+        x: feature.geometry.coordinates[0][0][0],
+        y: feature.geometry.coordinates[0][0][0],
+      });
+      for (let [x, y] of feature.geometry.coordinates[0]) {
+        const elevation = await Forma.terrain.getElevationAt({ x, y });
+
+        if (elevation < terrainElevation) {
+          terrainElevation = elevation;
+        }
+      }
+
+      const floorStack = {
+        floors: [
+          {
+            polygon: feature.geometry.coordinates[0],
+            height: feature.properties.height,
+          },
+        ],
+      };
+
+      const { urn } = await Forma.elements.floorStack.createFromFloors(floorStack);
+
+      // prettier-ignore
+      const transform = [
+        1, 0, 0, 0, 
+        0, 1, 0, 0, 
+        0, 0, 1, 0, 
+        0, 0, terrainElevation, 1
+      ];
+
+      await Forma.proposal.addElement({ urn, transform });
+    }
   } catch (error) {
     console.error("Error making API request:", error);
     // Handle errors
@@ -113,11 +175,25 @@ async function callChatGPT(prompt: string) {
 
 function AskGPT({ steps, triggerNextStep }) {
   const prompt = steps?.prompt?.value;
-  console.log(renderPrompt(prompt));
 
   useEffect(() => {
     (async function call() {
-      await callChatGPT(renderPrompt(prompt));
+      const site = await Forma.geometry.getFootprint({
+        path: (await Forma.selection.getSelection())[0],
+      });
+
+      const [x, y] = site?.coordinates[0];
+
+      let elevation = await Forma.terrain.getElevationAt({ x, y });
+      for (let [x, y] of site?.coordinates) {
+        const thisElevation = await Forma.terrain.getElevationAt({ x, y });
+
+        if (thisElevation < elevation) {
+          elevation = thisElevation;
+        }
+      }
+
+      await callChatGPT(renderPrompt(prompt, site?.coordinates, elevation));
       triggerNextStep();
     })();
   }, []);
@@ -153,7 +229,7 @@ export default function App() {
           {
             id: "4",
             message: "Yay!",
-            end: true,
+            trigger: "1",
           },
         ]}
       />
